409b13495e1541bb1d06eca4c39190f6a6efba49
Gilvan Alves Pastor Junior <gilvan.junior423@gmail.com>
2026-04-22 13:02:51 -0300

    Add 2936 question from beecrowd solution

---
+++ 2936.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int cur, boi, bot, map, iar;
+    cin >> cur >> boi >> bot >> map >> iar;
+    cout <<cur*300+boi*1500+bot*600+map*1000+iar*150+225 << "\n";
+return 0;
+}
